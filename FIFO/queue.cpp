ab9c7c520b181e46a34b34deff7dc3cd8c3465a1
ulyssis <li.di.china@gmail.com>
2024-06-04 16:53:10 +0200

    improve; add copy constructor and copy assignment operator

--- FIFO/queue.cpp
+++ FIFO/queue.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <utility>
 
 class Node
 {
@@ -23,8 +24,10 @@ private:
     int capacity;
 
 public:
+    // constructor
     Queue(int cap) : head(nullptr), tail(nullptr), size(0), capacity(cap) {}
 
+    // deconstructor
     ~Queue()
     {
         while (head != nullptr)
@@ -35,13 +38,55 @@ public:
         }
     }
 
+    // copy constructor
+    Queue(const Queue &other);
+
+    // copy assignment constructor
+    Queue &operator=(Queue &other);
+
     Node *dequeue();
     void queue(int val);
     int get_size();
     void print();
+    int get_capacity();
 };
 
-// copy assignment operator, move constructor, and move assignment operator
+// copy constructor
+Queue::Queue(const Queue &other) : head(nullptr), tail(nullptr), size(0), capacity(other.capacity)
+{
+    Node *current = other.head;
+    while (current != nullptr)
+    {
+        queue(current->val);
+        current = current->next;
+    }
+}
+
+// copy assignment constructor
+Queue &Queue::operator=(Queue &other)
+{
+    if (this != &other)
+    {
+        Node *current = head;
+        while (current != nullptr)
+        {
+            Node *temp = current;
+            current = current->next;
+            delete temp;
+        }
+        head = other.head;
+        tail = other.tail;
+        size = other.size;
+        capacity = other.capacity;
+
+        other.head = nullptr;
+        other.tail = nullptr;
+        other.size = 0;
+    }
+    return *this;
+}
+
+//  move constructor, and move assignment operator
 
 void Queue::print()
 {
@@ -59,6 +104,11 @@ void Queue::print()
         }
         std::cout << std::endl;
     }
+}
+
+int Queue::get_capacity()
+{
+    return this->capacity;
 }
 
 void Queue::queue(int val)
@@ -116,14 +166,29 @@ int main()
     }
 
     q->print();
+    std::cout << "capacity of q is " << q->get_capacity() << std::endl;
     std::cout << "size of the queue is: " << q->get_size() << std::endl;
 
     q->dequeue();
-    std::cout << "size of the queue is: " << q->get_size() << std::endl;
+    // std::cout << "size of the queue is: " << q->get_size() << std::endl;
+    q->dequeue();
+    // std::cout << "size of the queue is: " << q->get_size() << std::endl;
     q->dequeue();
     std::cout << "size of the queue is: " << q->get_size() << std::endl;
-    q->dequeue();
-    std::cout << "size of the queue is: " << q->get_size() << std::endl;
-    // Node *head = q->dequeue();
-    // std::cout << "value of head is: " << head->val << std::endl;
+
+    Queue *q2 = q;             // pointer is declared and assigned
+    Queue *q3 = new Queue(20); // trigger constructor
+    q3 = q;                    // pointer is reassigned
+    q2->print();
+    std::cout << "capacity of q2 is " << q2->get_capacity() << std::endl;
+    std::cout << "capacity of q3 is " << q3->get_capacity() << std::endl;
+
+    delete q;
+    // delete q3;
+
+    Queue q4 = *q;             // copy constructor is triggered
+    Queue *q5 = new Queue(30); // constructor is trigger
+    *q5 = *q;                  // copy assignment operator is triggered
+
+    return 0;
 }
